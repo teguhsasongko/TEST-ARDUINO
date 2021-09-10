2b759b6b9986522658624a22c81dfa45e2e4db9b
teguhsasongko.id <teguhsasongko@users.noreply.github.com>
2021-09-10 23:11:14 +0700

    UPDATE FIX
    UDAH BISA KONEKSI INPUT KE DATABASE

--- nyoba-dht22-nodemcu.ino
+++ nyoba-dht22-nodemcu.ino
@@ -1,23 +1,53 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
 String sentence;
-String status_;
 String suhu;
 String kelembapan;
 
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+const char* ssid = "localhost"; 
+const char* password = "";
+const char* host = "192.168.1.9";
+const int port = 80;
+
+WiFiClient client;
+  
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  while (!Serial) {
-  Serial.println("ERROR"); // wait for serial port to connect. Needed for native USB port only
+  while (!Serial) {; // wait for serial port to connect. Needed for native USB port only
   }
+  // Networking
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  if (!client.connect(host, port)) {
+    Serial.println("Connection failed");
+    return;
+  }
   // put your main code here, to run repeatedly:
   if (Serial.available()) {
+    
 //    Serial.write(Serial.read());
     sentence = Serial.readString();
-//    Serial.println(status_);
-//    Serial.println(sentence);
     suhu = String(getValue(sentence,' ',0));
     Serial.print("Suhu: ");
     Serial.println(suhu); 
@@ -26,8 +56,37 @@ void loop() {
     Serial.print("kelembapan:");
     Serial.println(kelembapan); 
     }
+    kirim();
+    delay(5000);
 }
+void kirim()   //CONNECTING WITH MYSQL
+ { 
+String apiUrl = "/lomba/kirim.php?";
+  apiUrl += "data4="+String(suhu);
+  apiUrl += "&data5="+String(kelembapan);
+  
+  // Set header Request
+  client.print(String("POST ") + apiUrl + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
 
+  // Pastikan tidak berlarut-larut
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 1000) {
+      Serial.println(">>> Client Timeout !");
+      Serial.println(">>> Operation failed !");
+      client.stop();
+      return;
+    }
+  }
+  // Baca hasil balasan dari PHP
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.println(line);
+  }             
+} 
+ 
 String getValue(String data, char separator, int index){
   int found = 0;
   int strIndex[] = {0,-1};
